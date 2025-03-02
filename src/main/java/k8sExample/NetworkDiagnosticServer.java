ce70b6575987355f0b634228716157723251696c
skcc-cwchoe <cwchoe@sk.com>
2025-03-02 15:17:17 +0900

    github packages 사용

--- src/main/java/k8sExample/NetworkDiagnosticServer.java
+++ src/main/java/k8sExample/NetworkDiagnosticServer.java
@@ -106,27 +106,29 @@ public class NetworkDiagnosticServer {
     }
 
 
-// Request DTO classes
-class NetcatRequest {
-    private String ip;
-    private String port;
-    
-    public String getIp() { return ip; }
-    public void setIp(String ip) { this.ip = ip; }
-    public String getPort() { return port; }
-    public void setPort(String port) { this.port = port; }
-}
+    // Request DTO classes
+    class NetcatRequest {
+        private String ip;
+        private String port;
+        
+        public String getIp() { return ip; }
+        public void setIp(String ip) { this.ip = ip; }
+        public String getPort() { return port; }
+        public void setPort(String port) { this.port = port; }
+    }
 
-class DnsRequest {
-    private String url;
-    
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
-}
+    class DnsRequest {
+        private String url;
+        
+        public String getUrl() { return url; }
+        public void setUrl(String url) { this.url = url; }
+    }
 
-class UrlRequest {
-    private String url;
-    
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
-} 
+    class UrlRequest {
+        private String url;
+        
+        public String getUrl() { return url; }
+        public void setUrl(String url) { this.url = url; }
+    } 
+
+}
